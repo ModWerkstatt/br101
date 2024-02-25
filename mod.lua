08b77f33232560cded160ff0666cd56a9bc4af7b
Florian Thiel <flth@flth.de>
2024-02-25 22:26:37 +0100

    Previewversion

--- mod.lua
+++ mod.lua
@@ -7,21 +7,85 @@ return {
 		name = _("mod_name"),
 		description = _("mod_desc"),
 		authors = {
-			{
-				name = "ModWerkstatt",
-				role = "CREATOR",
-			},
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
 		},
-		tags = { "europe", "train", "electric", "germany", "deutsche bahn" },
-		tfnetId = {},
+		tags = { "europe", "train", "siemens", "vectron", "smartron", "electric", "multi-system", "universal" },
+		minGameVersion = 0,
 		dependencies = { },
 		url = { "" },
+        params = {
+			{
+				key = "br101_vorspann",
+				name = _("vorspann_br101"),
+				values = { "No", "Yes", },
+				tooltip = _("option_vorspann_br101_desc"),
+				defaultIndex = 0,
+			},
+			{
+				key = "br101_fake",
+				name = _("fake_br101"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_br101_desc"),
+				defaultIndex = 0,
+			},
+			{
+				key = "br101_endless",
+				name = _("endjahr_br101"),
+				values = { "No", "Yes", },
+				tooltip = _("option_endjahr_br101_desc"),
+				defaultIndex = 0,
+			},
+        },
 	},
 	options = {
 	},
+	runFn = function (settings, modParams)
 
-	runFn = function (settings)
+		local vorspannFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.baureihe101 and data.metadata.br101.vorspann == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+				--return false
+			end
+			--return true
+			return data
+		end
 
+		local fakeFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br101 and data.metadata.br101.fake == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+			end
+			return data
+		end
+		local function endlessFilter(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br101 then
+                data.metadata.availability.yearTo = 0
+            end
+			return data
+		end
+
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["br101_vorspann"] == 0 then
+				--addFileFilter("model/transportVehicle", vorspannFilter)
+				addModifier("loadModel", vorspannFilter)
+			end
+			if params["br101_fake"] == 0 then
+				addModifier("loadModel", fakeFilter)
+			end
+            if params["br101_endless"] == 1 then
+                addModifier("loadModel", endlessFilter)
+            end
+
+		else
+			--addFileFilter("model/transportVehicle", vorspannFilter)
+			addModifier("loadModel", fakeFilter)
+			addModifier("loadModel", endlessFilter)
+		end
 	end
 }
 end
--- res/config/sound_set/br101.lua
+++ res/config/sound_set/br101.lua
@@ -1,4 +1,4 @@
-local bbs2util = require "bbs2util"
+local soundeffectsutil = require "soundeffectsutil"
 
 function data()
 return {
@@ -10,7 +10,7 @@ return {
 		{ name = "vehicle/br101/101_anf2.wav", refDist = 25.0 },
 		{ name = "vehicle/br101/kurve.wav", refDist = 20.0 },
 		{ name = "vehicle/br101/bremse_neu.wav", refDist = 25.0 },
-		{ name = "vehicle/br101/101_brems.wav", refDist = 25.0 }
+	--	{ name = "vehicle/br101/101_brems.wav", refDist = 25.0 }
 	},
 	events =  {
 		horn = { names = { "vehicle/br101/101_makro.wav" }, refDist = 25.0 }
@@ -18,75 +18,75 @@ return {
 
 	updateFn = function (input)
 		local axleRefWeight = 10.0
-		
+
 		return {
 			tracks = {
-				{ 
-					gain = bbs2util.sampleCurve({ 
+				{
+					gain = soundeffectsutil.sampleCurve({
 									{ .0, 0.75 },
 									{ .1, .0 },
 									{ 1.0, .0 } }, input.speed01),
-					pitch = bbs2util.sampleCurve({ 
+					pitch = soundeffectsutil.sampleCurve({
 									{ .0, 1.0 },
 									{ 1.0, 1.0 } }, input.speed01)
 				},
 				--luefter
-				{ 
-					gain = bbs2util.sampleCurve({ 
+				{
+					gain = soundeffectsutil.sampleCurve({
 									{ .0, .0 },
 									{ .149, .0 },
 									{ 0.15, 0.8 },
 									{ 1.0, 0.8 } }, input.speed01),
-					pitch = bbs2util.sampleCurve({ 
+					pitch = soundeffectsutil.sampleCurve({
 									{ .0, 1.0 },
 									{ 1.0, 1.0 } }, input.speed01)
 				},
 				--beschleunigung
-				{	 
-					gain = bbs2util.sampleCurve({ 
+				{
+					gain = soundeffectsutil.sampleCurve({
 										{ 0.0, 0.0 },
 										{ 0.15, 0.0 },
 										{ 0.16, 0.7 },
 										{ 1.0, 0.8 } }, input.power01),
-					pitch = bbs2util.sampleCurve({ 
+					pitch = soundeffectsutil.sampleCurve({
 										{ 0.0, 0.0 },
 										{ 0.19, 0.0 },
 										{ 0.2, 0.5 },
 										{ 1.0, 1.25 } }, input.speed01)
 				},
 				--anfahrt 1
-				{ 
-					gain = bbs2util.sampleCurve({ 
+				{
+					gain = soundeffectsutil.sampleCurve({
 									{ .0, 0.0 },
 									{ .01, 1.0 },
 									{ 0.15, 1.0 },
 									{ 0.161, 0.0 },
 									{ 1.0, 0.0 }}, input.speed01),
-					pitch = bbs2util.sampleCurve({ 
+					pitch = soundeffectsutil.sampleCurve({
 									{ .0, 0.9 },
 									{ .05, 1.0 },
 									{ .25, 1.0 },
 									{ .26, 0.0 },
 									{ 1.0, 0.0 } }, input.speed01)
-				},		
+				},
 				--anfahrt2
-				{ 
-					gain = bbs2util.sampleCurve({ 
+				{
+					gain = soundeffectsutil.sampleCurve({
 									{ .0, 0.0 },
 									{ .148, 0.0 },
 									{ .15, 1.25 },
 									{ .5, 0.0 },
-									{ 1.0,0.0 } }, input.speed01),								
-					pitch = bbs2util.sampleCurve({ 
+									{ 1.0,0.0 } }, input.speed01),
+					pitch = soundeffectsutil.sampleCurve({
 									{ .0, 0.0 },
 									{ .399, 0.0 },
 									{ .4, 1.0 },
 									{ 1.0, 1.0 } }, input.power01)
-				},				
+				},
 
-				bbs2util.squeal(input.speed, input.sideForce, input.maxSideForce),
-				bbs2util.brake(input.speed, input.brakeDecel, 1.2),
-				bbs2util.slow(input.speed, input.brakeDecel, 1.5)
+				soundeffectsutil.squeal(input.speed, input.sideForce, input.maxSideForce),
+				soundeffectsutil.brake(input.speed, input.brakeDecel, 1.2),
+			--	soundeffectsutil.slow(input.speed, input.brakeDecel, 1.5)
 			},
 			events = {
 			}
